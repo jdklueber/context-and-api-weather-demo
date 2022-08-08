213eab64f8c57b4542570e6e03b3845e701f6687
Jason Klueber <jason.klueber@gmail.com>
2022-08-08 13:34:36 -0400

    Tweaked button colors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 function Button({label, onClick}) {
     return (
-        <button className="m-2 p-2 text-sm border border-black rounded-full bg-slate-50"
+        <button className="m-2 p-2 text-sm text-blue-900 border border-blue-900 rounded-full bg-blue-100"
                 onClick={onClick}>{label}</button>
     )
 }
